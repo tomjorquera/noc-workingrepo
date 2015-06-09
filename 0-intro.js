3570a6e54021a2bf368f9b814daf3ec45b47743f
Tom Jorquera <tom@jorquera.net>
2015-06-09 21:11:59 +0200

    Walker is now biaised toward mouse
    The walker now mostly follow the mouse.
    In order for the effect to be visible, the drawing on the canvas now
    work on a delayed step-by-step basis.

--- 0-intro.js
+++ 0-intro.js
@@ -1,14 +1,17 @@
 function intro(canvasId) {
-    return {
+    var res = {
         canvas : document.getElementById(canvasId),
-        ctx : canvas.getContext('2d'),
+        ctx : this.canvas.getContext('2d'),
         walker : function() {
             return {
-                x : canvas.height/2,
-                y : canvas.width/2,
+                x : this.canvas.height/2,
+                y : this.canvas.width/2,
                 step : function() {
-                    var stepx = Math.random() * 2.5 -1;
-                    var stepy = Math.random() * 2.5 -1;
+                    var biasx = this.x > res.mousepos.x ? -0.5 : 0.5;
+                    var biasy = this.y > res.mousepos.y ? -0.5 : 0.5;
+
+                    var stepx = Math.random() * 2 -1 +biasx;
+                    var stepy = Math.random() * 2 -1 +biasy;
                     this.x = this.x + stepx;
                     this.y = this.y + stepy;
                 }
@@ -16,6 +19,16 @@ function intro(canvasId) {
         },
         display : function(walker) {
             this.ctx.fillRect(walker.x, walker.y, 1, 1);
-        }
+        },
+        mousepos : {x:0, y:0}
     };
+
+    res.canvas.onmousemove = function(evt) {
+        var rect = res.canvas.getBoundingClientRect();
+        res.mousepos = {
+            x: (evt.clientX-rect.left)/(rect.right-rect.left)*res.canvas.width,
+            y: (evt.clientY-rect.top)/(rect.bottom-rect.top)*res.canvas.height};
+    };
+
+    return res;
 }
