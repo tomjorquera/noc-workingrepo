05577d1e4fb2db4dc0ca78a457bd6d1b1daa532c
Tom Jorquera <tom@jorquera.net>
2015-06-14 22:16:07 +0200

    Separate bounce move and draw code
    Some refactoring to separate moving and drawing

--- 1-bounce.js
+++ 1-bounce.js
@@ -12,6 +12,18 @@ function bounce(canvasId) {
         ctx.fillRect(0, 0, canvas.width, canvas.height);
     };
 
+    res.move = function() {
+        vec2.add(this.xy, this.xy, this.speed);
+
+        if ((this.xy[0] > canvas.width) || (this.xy[0] < 0)) {
+            this.speed = vec2.fromValues(this.speed[0] * -1, this.speed[1]);
+        }
+        if ((this.xy[1] > canvas.height) || (this.xy[1] < 0)) {
+            this.speed = vec2.fromValues(this.speed[0], this.speed[1] * -1);
+        }
+
+    };
+
     function drawCircle(x,y) {
         ctx.beginPath();
         ctx.arc(x,y, 16, 0, 2 * Math.PI, false);
@@ -25,17 +37,12 @@ function bounce(canvasId) {
     res.draw = function() {
 
         this.setup();
-
-        vec2.add(this.xy, this.xy, this.speed);
-
-        if ((this.xy[0] > canvas.width) || (this.xy[0] < 0)) {
-            this.speed = vec2.fromValues(this.speed[0] * -1, this.speed[1]);
-        }
-        if ((this.xy[1] > canvas.height) || (this.xy[1] < 0)) {
-            this.speed = vec2.fromValues(this.speed[0], this.speed[1] * -1);
-        }
-
         drawCircle(this.xy[0], this.xy[1]);
+    };
+
+    res.step = function() {
+        this.move();
+        this.draw();
     };
 
     return res;
