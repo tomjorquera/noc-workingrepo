98474d178ae9750d16d87fdab0f82d3a0e8d4189
Tom Jorquera <tom@jorquera.net>
2015-07-06 21:22:01 +0200

    Change the way forces are applied to movers
    The way forces are applied to movers is now different.
    
    A force is defined as an object with a type, indicating the
    kind of force, and a function which takes a mover in input and
    returns a force vector.
    
    The idea is to pass the force functions to the movers, which
    will control how the forces are applied depending on their
    type (inversion of control).
    
    noc.forces now contains force constructors functions, which take in
    account some forces constants and return force objets (also a `custom`
    force has been added to simplify new force creation).
    
    `applyForce`, renommed `applyForceVec`, is no longer supposed to be
    used directly in the general case. The function `subjectTo`, which
    takes a force function is to be used instead. This function allows for a
    better control on how forces are applied to a mover (inversion of
    control).

--- noc.js
+++ noc.js
@@ -2,40 +2,74 @@
 
 var noc = {};
 
+// A force is defined as an object with a type, indicating the
+// kind of force, and a function which takes a mover in input and
+// returns a force vector.
+//
+// The idea is to pass the force functions to the movers, which
+// will control how the forces are applied depending on their
+// type (inversion of control).
+//
+// Here we define some constructors for classical forces. A force
+// constructor takes some global parameters (e.g. gravitational
+// constant) and returns a force (a force being a function to be
+// passed to the movers).
 noc.forces = {
-    // gravity is a directed force depending on a mass and a gravitational constant (default 1)
-    gravity: function(m, g = 1) {
-        return vec2.fromValues(0, g * m);
+
+    // a general constructor to build your own forces
+    custom: function(type, f) {
+        return { type, f };
+    },
+
+    // gravity is a directed force depending on a mass and a
+    // gravitational constant (default 1)
+    gravity: function(gravityConst = 1) {
+        return noc.forces.custom(
+            "GRAVITY",
+            (mover => {
+                return vec2.fromValues(0, gravityConst * mover.mass);
+            })
+        );
     },
 
     // friction is a directed force opposite to the velocity,
     // depending on a normal vector (default (1,1)) and scaled
     // to a friction coeff (default 1)
-    friction: function(v, f = 1, norm = vec2.fromValues(1,1)) {
-        var frictionMag = vec2.create();
-        vec2.scale(frictionMag, norm, f);
+    friction: function(frictionCoeff = 1, norm = vec2.fromValues(1,1)) {
+        return noc.forces.custom(
+            "FRICTION",
+            (mover => {
+                let frictionMag = vec2.create();
+                vec2.scale(frictionMag, norm, frictionCoeff);
 
-        var res = vec2.create();
-        vec2.normalize(res, v);
-        vec2.scale(res, res, -1);
-        vec2.multiply(res, res, frictionMag);
-        return res;
+                let res = vec2.create();
+                vec2.normalize(res, mover.vel);
+                vec2.scale(res, res, -1);
+                vec2.multiply(res, res, frictionMag);
+                return res;
+            })
+        );
     },
 
     // drag is a force opposite to the velocity, depending
     // on on the env drag coeff, the mover frontal area and
     // the env density.
-    drag: function(v, dragCoeff, frontalArea = 1,  density = 1) {
-        var res = vec2.create();
-        var scale_factor = -1/2 * dragCoeff * frontalArea * density;
+    drag: function(dragCoeff, frontalArea = 1, density = 1) {
+        return noc.forces.custom(
+            "DRAG",
+            (mover => {
+                let res = vec2.create();
+                let scale_factor = -1/2 * dragCoeff * frontalArea * density;
 
-        vec2.normalize(res, v);
-        vec2.multiply(res, res, v);
-        vec2.multiply(res, res, v);
+                vec2.normalize(res, mover.vel);
+                vec2.multiply(res, res, mover.vel);
+                vec2.multiply(res, res, mover.vel);
 
-        vec2.scale(res, res, scale_factor);
+                vec2.scale(res, res, scale_factor);
 
-        return res;
+                return res;
+            })
+        );
     }
 };
 
@@ -57,8 +91,21 @@ noc.mover = function(options = {}) {
     res.wrapping = options.wrapping;
 
     // directly apply a force vector to a mover
-    res.applyForce = function(forceVector) {
+    // note: in the general case, this function should not be used
+    // directly. The `subjectTo` function should be used instead.
+    res.applyForceVec = function(forceVector) {
         vec2.scaleAndAdd(this.acc, this.acc, forceVector, 1/this.mass);
+        // allow chaining
+        return this;
+    };
+
+    // Take a force and apply it to the mover.
+    // note: this function can be overrided to enable special
+    // behaviors when applying forces.
+    res.subjectTo = function(force) {
+        this.applyForceVec(force.f(this));
+        // allow chaining
+        return this;
     };
 
     // the mover wrap around borders at less than `margin` distance
