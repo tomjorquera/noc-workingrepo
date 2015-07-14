849c209aa4073c420b29ad4f51f4562280ab2392
Tom Jorquera <tom@jorquera.net>
2015-07-14 11:32:07 +0200

    Fountain parameters tweaking

--- test.js
+++ test.js
@@ -18,7 +18,7 @@ function test(sim) {
     let randomThrow = (random) => noc.forces.custom(
         "THROW",
         (mover) => {
-            let angle = Math.PI * 5/4 + random * Math.PI/2;
+            let angle = Math.PI * 11/8 + random * Math.PI/4;
             let force = vec2.fromValues(Math.cos(angle), Math.sin(angle));
             vec2.scale(force, force, 3);
             return force;
@@ -33,14 +33,32 @@ function test(sim) {
         {
             mass: 5,
             wrapping:false,
-            lifespan: 200,
+            lifespan: 180,
             render: function(renderer) {
+
+                let ctx = renderer.canvas.getContext('2d');
+                let gradient = ctx.createRadialGradient(
+                    this.loc[0],
+                    this.loc[1],
+                    0,
+                    this.loc[0],
+                    this.loc[1],
+                    this.mass
+                );
+
+                gradient.addColorStop(
+                    0, 'rgba(0,0,255,' + this.lifespan/180 + ')'
+                );
+                gradient.addColorStop(
+                    1, 'rgba(0,0,255,0)'
+                );
+
                 renderer.drawCircle(
                     this.loc[0],
                     this.loc[1],
                     this.mass,
-                    'rgba(0,0,255,' + this.lifespan/200 + ')',
-                    'rgba(0,0,0,' + this.lifespan/200 + ')'
+                    gradient,
+                    'rgba(0,0,255,0)'
                 );
             }
         },
@@ -51,7 +69,7 @@ function test(sim) {
             noc.forces.custom(
                 "CONTINUE_THROW",
                 (mover) => {
-                    if(mover.lifespan && mover.lifespan > 190) {
+                    if(mover.lifespan && mover.lifespan > 175) {
                         return mover.initalForce.f(mover);
                     } else {
                         return vec2.fromValues(0,0);
@@ -75,8 +93,10 @@ function test(sim) {
                         loc: mouse.pos(), mass:30
                     }, 1, 5, 5));
 
-                let p = mover.addParticle();
-                p.initalForce = randomThrow(Math.random());
+                for(var i = 0; i < 15; i++) {
+                    let p = mover.addParticle();
+                    p.initalForce = randomThrow(Math.random());
+                }
             }
         }
     };
