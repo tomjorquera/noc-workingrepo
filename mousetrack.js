b38c3c778bfdf039d96de5dfa19951e09b84b4cc
Tom Jorquera <tom@jorquera.net>
2015-07-15 21:23:24 +0200

    Remove gl-matrix dependencies
    Remove last dependencies on gl-matrix

--- mousetrack.js
+++ mousetrack.js
@@ -3,13 +3,13 @@ function mousetrack(canvasId) {
     let canvas = document.getElementById(canvasId);
 
     // monitor mouse position
-    let mousePosition = vec2.fromValues(0, 0);
+    let mousePosition = [0, 0];
     canvas.onmousemove = (evt) => {
         let rect = canvas.getBoundingClientRect();
 
-        mousePosition =  vec2.fromValues(
+        mousePosition = [
             (evt.clientX-rect.left)/(rect.right-rect.left)*canvas.width,
-            (evt.clientY-rect.top)/(rect.bottom-rect.top)*canvas.height);
+            (evt.clientY-rect.top)/(rect.bottom-rect.top)*canvas.height];
     };
 
     return {
