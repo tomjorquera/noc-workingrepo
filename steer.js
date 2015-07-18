5e2a26db8ba8d5925db141008dde97ac8383cb1d
Tom Jorquera <tom@jorquera.net>
2015-07-18 22:33:58 +0200

    Mover now slow down when approching the target
    Use steer optional slowdown distance parameter to make the mover slow
    down when approching the mouse.

--- steer.js
+++ steer.js
@@ -8,7 +8,7 @@ function steer(sim, mouse){
 
     return {
         step:(mover) => {
-            mover.subjectTo(noc.forces.steer(mouse.pos(), 0.5));
+            mover.subjectTo(noc.forces.steer(mouse.pos(), 0.5, 50));
             mover.subjectTo(noc.forces.drag(0.2));
         }
     };
