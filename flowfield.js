2ed7663848b7dc1ca297374c97bc13ddd83cc9e9
Tom Jorquera <tom@jorquera.net>
2015-07-19 14:58:44 +0200

    Add slight repultion between movers
    Movers now repulse each over when being too near. The location of the
    movers is memorized on the grid, movers look cases around their to know
    which other movers are around.
    In order to improve performances, add some ugly hack using rendering
    steps to reduce the number of iterations. The position of movers on the
    field is updated during rendering step.

--- flowfield.js
+++ flowfield.js
@@ -5,22 +5,10 @@ function flowfield(sim, renderer, perlin) {
 
     noise.seed(Math.random());
 
-    let nbMovers = 400;
-    for(let i = 0; i < nbMovers; i++) {
-        sim.addMover({
-            mass:5,
-            loc:vec2.fromValues(
-                Math.random() * sim.width,
-                Math.random() * sim.height
-            )
-        });
-    }
-
     let fieldResolution = 20,
         cols = sim.width/fieldResolution,
         rows = sim.height/fieldResolution,
         field = [];
-
 
     // populate field with force vectors
     for(let i = 0; i < cols; i++) {
@@ -54,6 +42,8 @@ function flowfield(sim, renderer, perlin) {
                 ctx.lineTo(xNR, yNR);
                 ctx.stroke();
             };
+
+            field[i][j].movers = [];
         }
     }
 
@@ -82,13 +72,67 @@ function flowfield(sim, renderer, perlin) {
         for(let i = 0; i < cols; i++) {
             for(let j = 0; j < rows; j++) {
                 field[i][j].render(renderer);
+
+                // reset movers positions in the field
+                field[i][j].movers.length = 0;
             }
         }
     };
+
+    let nbMovers = 400;
+    let moverViz = 1;
+    let moverRepulsion = 0.1;
+
+    for(let i = 0; i < nbMovers; i++) {
+        let runner = noc.mover({
+            mass:5,
+            loc:vec2.fromValues(
+                Math.random() * sim.width,
+                Math.random() * sim.height
+            )
+        });
+
+        runner.oldRender = runner.render;
+        runner.render = function(renderer) {
+            this.oldRender(renderer);
+
+            // add itself to the list of runners in this field position
+            let x = Math.floor(runner.loc[0]/fieldResolution);
+            let y = Math.floor(runner.loc[1]/fieldResolution);
+            field[x][y].movers.push(runner);
+        };
+
+        runner.repulsion = noc.forces.custom(
+            'REPULSION',
+            (m) => vec2.scale(vec2.create(),
+                              noc.forces.gravitational(
+                                  runner,
+                                  moverRepulsion
+                              ).f(m), -1)
+        );
+        sim.movers.push(runner);
+    }
 
     return {
         step:(mover) => {
             mover.subjectTo(forceField);
             mover.subjectTo(noc.forces.drag(0.1));
-        }};
+
+            // repulsion from mover too near of its position
+            let locX = Math.floor(mover.loc[0]/fieldResolution);
+            let locY = Math.floor(mover.loc[1]/fieldResolution);
+            for(let i = locY - moverViz; i <= locY + moverViz; i++) {
+                for(let j = locX - moverViz; j <= locX + moverViz; j++) {
+                    if(i > 0 && i < cols && j > 0 && j < rows) {
+                        let f = field[i][j].movers;
+                        for(var k = 0; k < f.length; k++) {
+                            if(mover != f[k]) {
+                                mover.subjectTo(f[k].repulsion);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    };
 }
