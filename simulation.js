1ef4dbc129318a500d72b8421a162e01dab9916a
Tom Jorquera <tom@jorquera.net>
2015-07-25 12:09:43 +0200

    Add path rendering handling to simulation
    Simulation now handles paths memorization and rendering.

--- simulation.js
+++ simulation.js
@@ -9,14 +9,27 @@ function simulation(renderer, width, height) {
 
     res.movers = [];
 
+    res.paths = [];
+
     res.addMover = function(parameters) {
-        this.movers.push(noc.mover(parameters));
+        var mover = noc.mover(parameters);
+        this.movers.push(mover);
+        return mover;
     };
 
+    res.addPath = function(begin, end, radius, render) {
+        var path = noc.path(begin, end, radius, render);
+        this.paths.push(path);
+        return path;
+    };
 
     res.step = function(moveFunction){
         renderer.setup(width, height);
-        for (var i = 0; i < this.movers.length; i++) {
+
+        for (var i = 0; i < this.paths.length; i++) {
+            this.paths[i].render(renderer);
+        }
+        for (i = 0; i < this.movers.length; i++) {
             let m = this.movers[i];
             moveFunction(m);
             m.update(width, height, 20);
